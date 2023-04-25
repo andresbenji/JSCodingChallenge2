130d723ee82fc14c4efbc0019e19e4d8d1fbc884
andresbenji <116680825+andresbenji@users.noreply.github.com>
2023-04-25 12:03:09 -0400

    added comments

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-//function that filters out negative numbers
+//function that filters out negative numbers (numbers that are less than 0)
 function removeNegative(numbers) {
   return numbers.filter(function (numbers) {
     return numbers > 0;
   });
 }
 
-let numbers = [1, -5, 3, -5, 6, -7, 5];
+//add values to the array numbers
+let numbers = [1, -5, 3, -5, 6, -7, 5, 10, -15];
 
+//output the function with the array as an argument
 console.log(removeNegative(numbers));
